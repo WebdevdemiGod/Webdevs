33f18506507e195584777b6979fc85eacfb733d5
Xander Jacob <124575346+WebdevdemiGod@users.noreply.github.com>
2023-10-09 01:11:35 +0800

    Contact List Updated
    Displays the API fetched

--- src5/ContactList.jsx
+++ src5/ContactList.jsx
@@ -7,22 +7,26 @@ export default function ContactList({ contact, editContact, deleteToHandler }) {
             <h2>NO LIST SO FAR</h2>
         ) : (
             <>
-            <h2>List: </h2>
+            <h2>List:</h2>
             <table>
                 <thead>
                 <tr>
+                    <th>ID</th>
                     <th>Name</th>
-                    <th>Contact</th>
                     <th>Age</th>
+                    <th>Contact Number</th>
+                    <th>Course</th>
                     <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 {contact.map((item) => (
                     <tr key={item.id}>
+                    <td>{item.id}</td>
                     <td>{item.name}</td>
-                    <td>{item.contact}</td>
                     <td>{item.age}</td>
+                    <td>{item.contact}</td>
+                    <td>{item.course}</td>
                     <td>
                         <button onClick={() => editContact(item.id)}>Edit</button>
                         <button onClick={() => deleteToHandler(item.id)}>Delete</button>
